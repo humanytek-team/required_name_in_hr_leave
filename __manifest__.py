8eb517b0e410e6f4201ae970040abf2e8cdd8b66
Moises Navarro <moisalejandro@gmail.com>
2022-03-08 11:20:29 -0600

    [INIT]

---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Required Name in HR Leave",
+    "version": "13.0.0.1.0",
+    "author": "HomebrewSoft",
+    "website": "https://homebrewsoft.dev",
+    "license": "LGPL-3",
+    "depends": [
+        "hr_holidays",
+    ],
+    "data": [
+        # security
+        # data
+        # reports
+        # views
+        "views/hr_leave.xml",
+    ],
+}
